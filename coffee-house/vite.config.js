d66fc71a7f2fb012d60fb9ca3f6859aa316a033b
Pogos Torosian <pogosstudio@gmail.com>
2026-07-09 12:42:32 +0400

    feat: add sourcemap for js

--- coffee-house/vite.config.js
+++ coffee-house/vite.config.js
@@ -4,6 +4,8 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   base: './',
   build: {
+    minify: false,
+    sourcemap: true,
     rolldownOptions: {
       input: {
         main: resolve(import.meta.dirname, 'index.html'),
